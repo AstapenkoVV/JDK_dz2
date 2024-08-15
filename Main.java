f3a6178d3bd397746ebde65f1b5e07abda4d8cb6
AstapenkoVV <AstapenkoVV@mail.ru>
2024-08-15 15:19:08 +0300

    create new program

---
+++ Main.java
@@ -0,0 +1,9 @@
+package ru.gb.lisson2_dz;
+
+public class Main {
+    public static void main(String[] args) {
+        SimpleGenericClass simpleGenericClass = new SimpleGenericClass();
+        simpleGenericClass.sumElements(5, 5);
+        simpleGenericClass.sumElements("Hello ", "World!");
+    }
+}
